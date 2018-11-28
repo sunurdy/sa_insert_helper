1381b569eab5a115f2909f1edcdf7eea9b6e106a
zhangmengyang <zhangmengyang@b.360.cn>
2018-11-28 12:03:29 +0800

    update

--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@
 from setuptools import setup, find_packages
 
 setup(
-        name='sa_insert_helper',
+        name='sa-insert-helper',
         version='0.0.1',
         keywords='bulk insert',
         description='a library for bulk insert',
+        long_description=open('README.md').read(),
         license='MIT License',
         url='https://github.com/sunurdy/sa_insert_helper',
         author='sunurdy',
